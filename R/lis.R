3517474ad6c8d96a0a0b15c9e11e7bbb9a004a65
Junkka <johan.junkka@gmail.com>
2014-04-26 21:50:30 +0200

    Add age_split fix lis param documentation

--- R/lis.R
+++ R/lis.R
@@ -1,12 +1,19 @@
-#' List Objects 
-#' 
+#' List Objects in Environment
+#'
 #' Shows objects name and class in local environment. An extention of base::ls.
+#' @param order_by = "class" Order by "class" or "object"
+#' @export
 #' @return A matrix containing name and class of objects
 #' @author Johan Junkka
-#' @export
 
-lis <- function(){
-  xobjectsinenv <- cbind(sapply(ls(envir=.GlobalEnv),function(xaobject){class(get(xaobject))})) 
-  if (nrow(xobjectsinenv) <= 0) xobjectsinenv <- 'Empty environment'
-  return(xobjectsinenv)
+lis <- function (order_by="class") 
+{
+    xobjectsinenv <- as.data.frame(cbind(sapply(ls(envir = .GlobalEnv), function(xaobject) {
+        class(get(xaobject))
+    })))
+    if (nrow(xobjectsinenv) <= 0) 
+      xobjectsinenv <- "Empty environment"
+    x <- data.frame("object" = rownames(xobjectsinenv), "class" = xobjectsinenv[,1])
+    x <- x[order(x[,c(order_by)]), ]
+    print(x, row.names=FALSE)
 }

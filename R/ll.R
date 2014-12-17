d4027efcaa663101aba50f3768238f59b14a2dd5
junkka <johan.junkka@gmail.com>
2014-12-17 21:39:23 +0100

    New ll

--- R/ll.R
+++ R/ll.R
@@ -1,22 +1,34 @@
-#' List Objects in Environment
+#' List objects in environment
 #'
-#' Shows objects name and class in local environment. An extention of base::ls.
-#' @param order_by = "class" Order by "class" or "object"
+#' With size and dimensions
+#'
+#' @param ... pass to \code{.ls.object}
+#' @param n number of objects to display
+#' @source \url{http://stackoverflow.com/questions/1358003}
 #' @export
-#' @return A matrix containing name and class of objects
-#' @author Johan Junkka
 
-ll <- function (order_by="class") 
-{
-    xobjectsinenv <- as.data.frame(cbind(sapply(ls(envir = .GlobalEnv), 
-      function(xaobject) {
-        x <- class(get(xaobject))
-        x[1]
-      }
-    )))
-    if (nrow(xobjectsinenv) <= 0)
-      xobjectsinenv <- "Empty environment"
-    x <- data.frame("object" = rownames(xobjectsinenv), "class" = xobjectsinenv[,1])
-    x <- x[order(x[,c(order_by)]), ]
-    print(x, row.names=FALSE)
+ll <- function(..., n = 10) {
+    .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
+
+.ls.objects <- function (pos = 1, pattern, order.by,
+                        decreasing=FALSE, head=FALSE, n=5) {
+    napply <- function(names, fn) sapply(names, function(x)
+                                         fn(get(x, pos = pos)))
+    names <- ls(pos = pos, pattern = pattern)
+    obj.class <- napply(names, function(x) as.character(class(x))[1])
+    obj.mode <- napply(names, mode)
+    obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+    obj.size <- round(napply(names, object.size)/1000, 1)
+    obj.dim <- t(napply(names, function(x)
+                        as.numeric(dim(x))[1:2]))
+    vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+    obj.dim[vec, 1] <- napply(names, length)[vec]
+    out <- data.frame(obj.type, obj.size, obj.dim)
+    names(out) <- c("Type", "Size", "Rows", "Columns")
+    if (!missing(order.by))
+        out <- out[order(out[[order.by]], decreasing=decreasing), ]
+    if (head)
+        out <- head(out, n)
+    out
 }

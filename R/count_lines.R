d7ab18029eb4351bfe25a29c5b10a7835912e505
junkka <johan.junkka@gmail.com>
2017-04-06 12:56:59 +0200

    seperate folder & file in count lines

--- R/count_lines.R
+++ R/count_lines.R
@@ -12,8 +12,10 @@
 count_lines <- function(path = "."){
   x <- system(paste0("find ", path," -name '*.R' | xargs wc -l"), intern = TRUE)
   lines <- as.integer(gsub("^( )*([0-9]*)(.*)", "\\2", x))
-  file <- gsub("^( )*[0-9]* ", "", x)
-  res <- data.frame(file, lines, stringsAsFactors = FALSE)
+  path <- gsub("^( )*[0-9]* ", "", x)
+  folder <- stringr::str_replace(path, "[a-zA-Z0-9_-]*\\.R$", "")
+  file   <- stringr::str_extract(path, "[a-zA-Z0-9_-]*\\.R$")
+  res <- data.frame(path, folder, file, lines, stringsAsFactors = FALSE)
   message(paste("Total:", res$lines[nrow(res)]))
   res[-nrow(res), ]
 }
